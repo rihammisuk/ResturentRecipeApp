d114be2595ef89deb70ded35fc516c2d82fdf760
rihammisuk <riham659misuk@gmail.com>
2024-01-02 12:53:38 +0600

    3 - Course Project The Basics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simple a test',
+    'https://mojo.generalmills.com/api/public/content/TlHSx2qFaUKqqRn6IbqoPQ_gmi_hi_res_jpeg.jpeg?v=b576e5a0&t=466b54bb264e48b199fc8e83ef1136b4'),
+    new Recipe('B Test Recipe', 'This is simple a test',
+    'https://mojo.generalmills.com/api/public/content/TlHSx2qFaUKqqRn6IbqoPQ_gmi_hi_res_jpeg.jpeg?v=b576e5a0&t=466b54bb264e48b199fc8e83ef1136b4')
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+}
